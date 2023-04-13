2286f347c2df0db51fef26b2901fe263d3406560
meltampsaol <meltamps@aol.com>
2023-04-13 16:03:47 -0700

    save

--- src/main/java/uswo/inc/uswofinal/repository/BalancesRepository.java
+++ src/main/java/uswo/inc/uswofinal/repository/BalancesRepository.java
@@ -17,6 +17,6 @@ public interface BalancesRepository extends JpaRepository<Balances,Integer> {
     List<Balances> GET_ALL_BALANCES(String search_string);
 
      
-    @Query("SELECT a.lokal.lcode, a.foryear, a.balance AS startingbalance, SUM(IFNULL(payment.amount, 0)) AS currentpayment, SUM(a.balance - IFNULL(payment.amount, 0)) AS currentbalance FROM Subscription a LEFT JOIN payment ON a.lokal.lcode = payment.lcode AND a.`foryear`=payment.`foryear` AND a.did = payment.did INNER JOIN Lokal l ON l.lcode = a.lokal.lcode WHERE l.locale LIKE :locale GROUP BY a.lokal.lcode, a.foryear")
+    @Query("SELECT a.lokal.lcode, a.foryear, a.balance AS startingbalance, SUM(IFNULL(Payment.amount, 0)) AS currentpayment, SUM(a.balance - IFNULL(Payment.amount, 0)) AS currentbalance FROM Subscription a LEFT JOIN Payment ON a.lokal.lcode = Payment.lokal.lcode AND a.`foryear`=Payment.`foryear` AND a.did = Payment.did INNER JOIN Lokal l ON l.lcode = a.lokal.lcode WHERE l.locale LIKE :locale GROUP BY a.lokal.lcode, a.foryear")
     List<Balances> getBalances2(String locale);
 }
