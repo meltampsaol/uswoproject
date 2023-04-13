f7e7e335ef793b80af01b188e96a162618c05d20
meltampsaol <meltamps@aol.com>
2023-04-13 16:27:52 -0700

    save

--- src/main/java/uswo/inc/uswofinal/repository/BalancesRepository.java
+++ src/main/java/uswo/inc/uswofinal/repository/BalancesRepository.java
@@ -6,16 +6,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 
-
 import uswo.inc.uswofinal.model.Balances;
 
-
-public interface BalancesRepository extends JpaRepository<Balances,Integer> {
-
+public interface BalancesRepository extends JpaRepository<Balances, Integer> {
 
     @Procedure(procedureName = "get_all_balances")
     List<Balances> GET_ALL_BALANCES(String search_string);
 
-    @Query("SELECT a.getLokal().getLocale(), a.foryear, a.balance AS startingbalance, SUM(IFNULL(payment.amount, 0)) AS currentpayment, SUM(a.balance - IFNULL(payment.amount, 0)) AS currentbalance FROM Subscription a LEFT JOIN Payment ON a.getLokal().getLcode() = payment.getLokal().getLcode() AND a.foryear = Payment.foryear AND a.did = Payment.did INNER JOIN Lokal l ON l.lcode = a.getLokal().getLcode() WHERE l.locale LIKE :locale GROUP BY a.getLokal().getLcode(), a.foryear")
+    @Query("SELECT a.getLokal().getLocale()," +
+            "a.balance AS startingbalance," +
+            "SUM(IFNULL(payment.amount, 0)) AS currentpayment," +
+            "SUM(a.balance - IFNULL(payment.amount, 0)) AS currentbalance" +
+            "FROM Subscription a LEFT JOIN Payment ON a.getLokal().getLcode() = payment.getLokal().getLcode()" +
+            "AND a.foryear = Payment.foryear AND a.did = Payment.did INNER JOIN Lokal l ON l.lcode = a.getLokal().getLcode()"
+            +
+            "WHERE l.locale LIKE :locale GROUP BY a.getLokal().getLcode(), a.foryear")
     List<Balances> getBalances2(String locale);
 }
